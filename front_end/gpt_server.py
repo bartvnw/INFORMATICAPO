3902e4ef7eaf31c9078aa060f8bdfb509b0df879
Sylb1262 <126561@edu.maaswaalcollege.nl>
2024-03-28 22:56:57 +0100

    HIJ DOET HET EINDELIJK
    Hij werkt na de port van de server en de website te veranderen, en na flask_cors te implementeren.

--- front_end/gpt_server.py
+++ front_end/gpt_server.py
@@ -1,6 +1,8 @@
 from flask import Flask, jsonify
+from flask_cors import CORS
 
 app = Flask(__name__)
+CORS(app)
 
 @app.route('/front_end/gpt_website.html/d')
 def pindex():
@@ -12,5 +14,5 @@ def index():
     return jsonify({'message': 'Flask server is running and message is sent'})
 
 if __name__ == '__main__':
-    app.run(host='localhost', port=5500, debug=True)
-    
+    app.run(host='localhost', port=5000, debug=True)
+
