917c85b0a8ecd1d2c25bf5b07cdd85d490d80847
bartvnw <126696@edu.maaswaalcollege.nl>
2024-03-29 10:54:49 +0100

    onnodige regel weg

--- front_end/classificatie.py
+++ front_end/classificatie.py
@@ -13,6 +13,3 @@ def voorspel(afbeelding): #afbeelding is de afbeelding die de gebruiker uploadt
     resultaten = model.predict(preprocessed_voor_model) #voorspelt de waarde van de afbeelding. deze functie geeft een list terug met 10 waardes, voor de 10 klasses. de index van elke klasse die het niet is, is 0 en de klasse die het model wel denkt is 1.
     getal = int(np.argmax(resultaten)) #omdat de cijfers 0 tm 9 geclassificeert worden, is de index met de hoogste waarde ook automatisch het voorspelde cijfer. argmax geeft de hoogste waarde uit een lijst of array terug als numpy int64, die hier wordt omgezet naar een normale python int.
     return getal, preprocessed_afbeelding #deze hele functie geeft dus het voorspelde cijfer terug, en de preprocessed afbeelding.
-
-testt = voorspel('test1.jpg')[0]
-print(testt)
