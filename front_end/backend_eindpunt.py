f92d52e4eec8d6dfad2a09ffea7488030b1bb114
Sylb1262 <126561@edu.maaswaalcollege.nl>
2024-03-29 16:00:07 +0100

    Server werkt officieel!

--- front_end/backend_eindpunt.py
+++ front_end/backend_eindpunt.py
@@ -7,6 +7,7 @@ def backend_programma(afbeelding):
     afbeelding_bestand = afbeelding
     afbeelding = cv2.imdecode(np.frombuffer(afbeelding_bestand.read(), np.uint8), cv2.IMREAD_COLOR) #maakt van de Flask afbeelding een bestand wat opencv kan lezen
     resultaat = voorspel(afbeelding) #gebruikt de voorspelfunctie om de afbeelding te classificeren
+    return int(1)
 
 #resultaat[0] is een integer die terug gestuurd moet worden, en resultaat[1] is een afbeelding als numpy array, die moet denk ik nog omgezet worden voordat shit werkt
 #deze 2 moeten allebei terug gestuurd worden naar de server silly :3
